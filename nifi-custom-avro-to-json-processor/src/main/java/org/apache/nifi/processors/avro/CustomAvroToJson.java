20652fd1c3c13ee8165e96bedc2d208d5ab8b410
rahulkg31 <rahul199431@gmail.com>
2023-12-15 15:43:29 +0530

    processor implementation

--- nifi-custom-avro-to-json-processor/src/main/java/org/apache/nifi/processors/avro/CustomAvroToJson.java
+++ nifi-custom-avro-to-json-processor/src/main/java/org/apache/nifi/processors/avro/CustomAvroToJson.java
@@ -14,7 +14,6 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.regex.Pattern;
 
 import org.apache.avro.Schema;
 import org.apache.avro.generic.GenericData;
